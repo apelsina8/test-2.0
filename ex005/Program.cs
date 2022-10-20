db435ad377800fa93a0856d64e9b7eecd0ca79db
altnet <altnet@inbox.ru>
2022-10-20 17:40:10 +0300

    5

---
+++ ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Маша");
+{
+    Console.writeLine("Ура, это же МАША");
+ }
+else
+{
+    Console.Write("Привет, ");
+    COnsole.WriteLine(username);
+} 
